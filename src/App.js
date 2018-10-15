aadfc691f5404b58f479fc429d0fb5cae707da10
irvis <irvis.isaac@gmail.com>
2018-10-15 16:34:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Person from './components/Person/Person';
 import './App.css';
 
 class App extends Component {
+
+  state={
+    persons: [
+      {name : 'Jesse', age: 19},
+      {name : 'Woody', age: 32},
+      {name : 'Buzz', age: 28},
+    ],
+    showPerson : false
+  }
+
+  cambiarNombre = () =>{
+    this.setState({persons: [
+      {name : 'Lotso', age: 19},
+      {name : 'Barbie', age: 32},
+      {name : 'Ken', age: 28},
+    ]})
+  }
+
+  togglePersonHandler = () =>{
+    const doesntShow = this.state.showPerson
+    this.setState({showPerson : !doesntShow})
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Hola mundo</p>
+        <button onClick={this.togglePersonHandler}>Toggle</button>
+
+        {this.state.showPerson ? 
+         <div>
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+         </div> :  null
+      }
+       
+        
       </div>
     );
   }
